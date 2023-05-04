1529b05f6d368faae73b08eb63cb02b5fd05a6fa
Lukas-Wierdsma <Lukas.Wierdsma@gmail.com>
2023-05-04 14:32:47 +0100

    added output for ease of viewing

--- yaml/config.py
+++ yaml/config.py
@@ -7,5 +7,9 @@ with open("config.yml", "r") as ymlfile:
 
 for section in cfg:
     print(section)
+    # mysql
+    # other
 print(cfg["mysql"])
+# {'host': 'localhost', 'user': 'root', 'passwd': 'my secret password', 'db': 'write-math'}
 print(cfg["other"])
+# {'preprocessing_queue': ['preprocessing.scale_and_center', 'preprocessing.dot_reduction', 'preprocessing.connect_lines'], 'use_anonymous': True} """
